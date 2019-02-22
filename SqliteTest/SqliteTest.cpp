243f9692ef013bda9eff21cd04c6edd6a086e3dc
daixian <amano_tooko@qq.com>
2019-02-22 19:37:52 +0800

    决定还是使用vcpkg做包管理,然后暂时舍弃原来增加md的编译配置.将来更清楚再使用限制md

---
+++ SqliteTest/SqliteTest.cpp
@@ -0,0 +1,13 @@
+﻿// VSCMake.cpp: 定义应用程序的入口点。
+//
+
+#include "SqliteTest.h"
+#include <sqlite3.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	printf("%s\n", sqlite3_libversion());
+	return 0;
+}
