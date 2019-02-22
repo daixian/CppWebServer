f40424e866a8f061eaecf7dbd740026d2522c75b
daixian <amano_tooko@qq.com>
2019-02-22 21:28:07 +0800

    准备提交下试试Linux

--- SqliteTest/SqliteTest.cpp
+++ SqliteTest/SqliteTest.cpp
@@ -1,13 +1,27 @@
-﻿// VSCMake.cpp: 定义应用程序的入口点。
-//
-
-#include "SqliteTest.h"
+﻿#include "SqliteTest.h"
 #include <sqlite3.h>
 #include <stdio.h>
 using namespace std;
 
 int main()
 {
-	printf("%s\n", sqlite3_libversion());
-	return 0;
+    printf("%s\n", sqlite3_libversion());
+
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return (0);
+    }
+    else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
+
+    getchar();
+    return 0;
 }
