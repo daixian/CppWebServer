37bb638486e044027a9a2cc2cc7d937a55f7c128
daixian <amano_tooko@qq.com>
2019-02-22 15:12:03 +0800

    试了下使用cmake,但是未能使用到vckpg

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/array.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 
@@ -9,12 +10,17 @@ using namespace std;
 
 int main()
 {
-    boost::array<int,4> arr ={{1,2,3,4}};
-    for(size_t i = 0; i < arr.size(); i++)
+    boost::array<int, 4> arr = {{1, 2, 3, 4}};
+    for (size_t i = 0; i < arr.size(); i++)
     {
-        cout<< arr[i];
+        cout << arr[i];
     }
-    cout<<endl;
-    return 0;
 
+    boost::filesystem::directory_iterator di("./");
+    for (auto& item : di)
+    {
+        cout << item.path().string() << endl;
+    }
+    cin.get();
+    return 0;
 }
